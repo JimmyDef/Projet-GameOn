144ddbce8af4b173a6ee09af3502b1f7f9590256
jimmyDef <jimmydef@outlook.fr>
2023-09-24 23:25:38 +0200

    Issue #1 : Amélioration css + fermer la modale

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,16 +1,35 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+// function editNav() {
+//   const x = document.getElementById("myTopnav");
+//   if (x.className === "topnav") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
+// -------------------
+// Burger Menu
+// -------------------
+
+const burgerIcons = document.querySelectorAll(".nav-icon");
+
+// console.log("🚀 ~ navList:", navList);
+
+burgerIcons.forEach((elt) => {
+  elt.addEventListener("click", function () {
+    document.getElementById("myTopnav").classList.toggle("responsive");
+    burgerIcons.forEach((item) => {
+      item.classList.toggle("active");
+    });
+  });
+});
 
 // DOM Elements
+
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn = document.querySelectorAll(".btn-signup");
 const formData = document.querySelectorAll(".formData");
+const closeIcon = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +38,8 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+closeIcon.addEventListener("click", closeModale);
 
-
+function closeModale() {
+  modalbg.style.display = "none";
+}
